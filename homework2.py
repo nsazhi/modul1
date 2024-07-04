939dd6d31bf235e6a1a9291d629c71d9beb46bb6
nsazhi <n190275@inbox.ru>
2024-07-04 15:14:50 +0400

    homework2.py

--- homework2.py
+++ homework2.py
@@ -2,4 +2,4 @@ done_tasks=12
 hour=1.5
 course='Python'
 time_of_task=hour / done_tasks
-print('Курс:',course,', всего задач:',done_tasks,', затрачено часов:',hour,', среднее время выполнения:',time_of_task,' часа.')
+print('Курс:',course,', всего задач:',done_tasks,', затрачено часов:',hour,', среднее время выполнения:',time_of_task,'часа.')
