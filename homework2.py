72d0416948c90183e2c199ab241a529d10d8a0cf
nsazhi <n190275@inbox.ru>
2024-07-04 15:12:54 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+done_tasks=12
+hour=1.5
+course='Python'
+time_of_task=hour / done_tasks
+print('Курс:',course,', всего задач:',done_tasks,', затрачено часов:',hour,', среднее время выполнения:',time_of_task,' часа.')
